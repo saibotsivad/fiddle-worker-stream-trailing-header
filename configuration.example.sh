191ef44e8d4efe11fd11bb4f14aa02fc7360b310
Tobias Davis <accounts@davistobias.com>
2022-10-19 22:24:29 -0500

    Initial commit

---
+++ configuration.example.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Copy this file:
+#
+#   cp configuration.example.sh configuration.sh
+#
+# Go here and generate a token, if you don't have one already: https://dash.cloudflare.com/profile/api-tokens
+#
+export CF_API_TOKEN="veryLongAPIToken"
+export CF_ACCOUNT_ID="123"
+export CF_ZONE_ID="456"
